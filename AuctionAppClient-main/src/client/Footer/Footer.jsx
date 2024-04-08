b015f6f9767d6cf4deca37936b31fda628e7a279
nithin <nithinreddy0187@gmail.com>
2024-04-08 14:08:54 +0530

    client code

---
+++ AuctionAppClient-main/src/client/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './footer.scss'; // Import your footer styles here
+import { FaGithub } from 'react-icons/fa';
+
+const teamMembers = [
+  { name: 'Nithin', github: 'https://github.com/Nithinreddy00' },
+  { name: 'Sreehitha', github: 'https://github.com/Sreehitha07' },
+  { name: 'Vishnu', github: 'https://github.com/' },
+];
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-info">
+            <h3>Created & Developed by Team</h3>
+          </div>
+          <div className="team-members">
+            {teamMembers.map((member, index) => (
+              <div key={index}>
+                <FaGithub />
+                <span className="dark-color">
+                  <a href={member.github} target="_blank" rel="noopener noreferrer">
+                    {member.name}
+                  </a>
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p className="light-color">&copy; 2023 auction app. All Rights Reserved</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
